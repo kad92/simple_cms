6701e1167ba168027fbdce5b7a1f224a5e3674c4
kad92 <kadiatou_diallo@hotmail.com>
2019-08-28 14:36:46 -0700

    Added edit and delete features for admin users

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -15,7 +15,7 @@ class AdminUsersController < ApplicationController
       @admin_user = AdminUser.new(admin_user_params)
 
       if @admin_user.save
-        flash[:notice] = "Admin created successfully!"
+        flash[:notice] = "#{@admin_user.name}created successfully!"
         redirect_to(:action => 'index')
       else
         render 'new'
@@ -23,15 +23,40 @@ class AdminUsersController < ApplicationController
     end
 
     def edit
+      @admin_user = AdminUser.find(params[:id])
     end
 
     def update
+      @admin_user = AdminUser.find(params[:id])
+
+      if @admin_user.update_attributes(admin_user_params)
+        flash[:notice] = "#{@admin_user.first_name}'s info have been updated successfully."
+        redirect_to(:action => 'index')
+      else
+        render 'edit'
+      end
+
     end
 
     def delete
+      @admin_user = AdminUser.find(params[:id])
     end
 
     def destroy
+      admin_user = AdminUser.find(params[:id])
+
+      admin_user.destroy
+
+      flash[:notice] = "#{admin_user.name} has been deleted from admin users."
+
+      if admin_user.id == session[:user_id]
+        session[:user_id] = nil
+        session[:username] = nil
+
+        redirect_to(:controller => 'access',:action => 'login')
+      else
+        redirect_to(:action => 'index')
+      end
     end
 
     private
